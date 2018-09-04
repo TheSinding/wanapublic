d9ffd14003a38ce2a603df0cb567d78d98b1ba54
Simon Sinding <sinding2000@gmail.com>
2018-09-04 16:17:53 +0200

    fixed

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <h1>404 this is here</h1>
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-lg-7 my-auto">
+          <div class="header-content mx-auto">
+            <h1 class="mb-5">Ooops, you are in the wrong place my friend!</h1>
+          </div>
+        </div>
+      </div>
     </div>
+  </header>
 </template>
+<style scoped>
+.masthead {
+  color: white;
+  background: #957bbe;
+  height: 100vh;
+}
+.code-example {
+  border-radius: 8px;
+}
+</style>
