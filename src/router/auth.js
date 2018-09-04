ba8f9a3d8344ac1cc19e695c805be8da014e0ff5
Simon Sinding <sinding2000@gmail.com>
2018-09-04 21:04:13 +0200

    added protected routes

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,29 +1,14 @@
+import store from '../store/'
+
 export default (to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (localStorage.getItem('wana-jwt') == null) {
-      next({
-        path: '/login',
-        params: { nextUrl: to.fullPath }
-      })
-    } else {
-      let user = JSON.parse(localStorage.getItem('user'))
-      if (to.matched.some(record => record.meta.is_admin)) {
-        if (user.is_admin === 1) {
-          next()
-        } else {
-          next({ name: 'userboard' })
-        }
-      } else {
-        next()
-      }
-    }
-  } else if (to.matched.some(record => record.meta.guest)) {
-    if (localStorage.getItem('jwt') == null) {
-      next()
-    } else {
-      next({ name: 'userboard' })
-    }
-  } else {
-    next()
+  const { auth } = store.state
+  const { user, authenticated } = auth
+
+  if (to.meta.requiresAuth && !authenticated) return next('/login')
+
+  if (to.meta.requiresAdmin && !to.meta.rank.includes(user.rank)) {
+    return next('/404')
   }
+
+  next()
 }
